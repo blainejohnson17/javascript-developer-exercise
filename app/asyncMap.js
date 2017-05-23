5476f4aacc229de6e119cdc55c5042437ececba3
Blaine Johnson <blainejohnson17@gmail.com>
2017-05-23 17:08:27 -0600

    update asyncMap

--- app/asyncMap.js
+++ app/asyncMap.js
@@ -1,7 +1,18 @@
 Array.prototype.asyncMap = function(transFunc, cb) {
-  this.forEach(function(e) {
-    setTimeout(function() {
-      cb(transFunc(e))
-    }, 0)
-  });
+  var self = this;
+  var results = [];
+  function job(i) {
+    results[i] = transFunc(self[i]);
+    if (i < self.length - 1) {
+      setTimeout(job, 0, i + 1);
+    } else {
+      cb(results);
+    }
+  }
+
+  setTimeout(job, 0, 0);
 };
+
+
+
+[1, 2, 3].asyncMap(item => (item + 1), result => console.log(result))
